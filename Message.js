8f23fb13ec364b17b4e746755086c3f89649ba9f
Smitth555 <136257135+Smitth555@users.noreply.github.com>
2023-07-22 18:19:36 +0500

    Add files via upload

--- Message.js
+++ Message.js
@@ -33,19 +33,38 @@ console.log(message);
 // Получаем кнопку по ее идентификатору
 var button = document.getElementById('orderButton');
 
-// Функция, которая будет вызываться при нажатии на кнопку
-function handleClick(event) {
-
-  // Вставьте здесь код, который должен выполняться при нажатии на кнопку
-  alert('Ваш заказ оформлен!');
-}
-
 // Назначаем обработчик события на нажатие кнопки
 button.addEventListener('click', handleClick);
+
 function handleClick(event) {
+  // Проверяем, все ли обязательные поля формы заполнены
+  var inputs = document.querySelectorAll('#tg input[required]');
+  var isFormValid = true;
+  for (var i = 0; i < inputs.length; i++) {
+    if (!inputs[i].value) {
+      isFormValid = false;
+      break;
+    }
+  }
+
+  if (isFormValid) {
     var message = document.createElement('div');
-    message.innerHTML = 'Ваш заказ оформлен !';
+    message.innerHTML = 'Ваш заказ оформлен ждите ответа!';
     message.classList.add('message');
     document.body.appendChild(message);
-  }
+  } 
+}
+document.getElementById("tg").addEventListener("submit", function(event){
+  document.getElementById("orderButton").disabled = true; // Отключение кнопки отправки
+
+  // Вывод сообщения о запрете повторной отправки
+  var message1 = document.createElement("div");
+  message1.id = "message1"; // Добавление идентификатора "message1" к элементу
+  message1.innerHTML = "Форма уже отправлена. Повторная отправка запрещена.";
+  document.body.appendChild(message1);
+
+  event.preventDefault(); // Предотвращает фактическую отправку формы
+});
+
+
   
